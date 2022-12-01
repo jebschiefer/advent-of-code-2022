7a5cd78cc641c0acea2f2eabb888c8e9f9776037
Jeb Schiefer <jeb.schiefer@modea.com>
2022-12-01 17:41:54 -0500

    Add test for day 1 part 1

--- test/day01_test.exs
+++ test/day01_test.exs
@@ -21,4 +21,9 @@ defmodule Day01.Test do
 
     assert Day01.find_most_calories(input) == 24000
   end
+
+  test "day 1 part 1" do
+    {:ok, content} = File.read("test/day01_input.txt")
+    assert Day01.find_most_calories(content) == 68467
+  end
 end
