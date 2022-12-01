b12a0090276c4bf85de15ba04923f5a3d55059f5
Jeb Schiefer <jeb.schiefer@modea.com>
2022-12-01 17:33:15 -0500

    Implement day 1 example

--- lib/day_01.ex
+++ lib/day_01.ex
@@ -1,5 +1,15 @@
 defmodule Day01 do
     def find_most_calories(input) do
-      0
+      input
+      |> String.split("\n\n")
+      |> Enum.map(&count_calories/1)
+      |> Enum.max
+    end
+
+    def count_calories(calories) do
+      calories
+      |> String.split("\n", trim: true)
+      |> Enum.map(&String.to_integer/1)
+      |> Enum.reduce(fn x, acc -> x + acc end)
     end
 end
