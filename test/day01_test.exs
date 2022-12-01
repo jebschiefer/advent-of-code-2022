250b13e89e8bc96f4dbad80daac7093500486ed0
Jeb Schiefer <jeb.schiefer@modea.com>
2022-12-01 17:46:39 -0500

    Add test for day 1 part 2 example

--- test/day01_test.exs
+++ test/day01_test.exs
@@ -1,29 +1,33 @@
 defmodule Day01.Test do
   use ExUnit.Case
 
+  @example_input """
+  1000
+  2000
+  3000
+
+  4000
+
+  5000
+  6000
+
+  7000
+  8000
+  9000
+
+  10000
+  """
+
   test "day 1 part 1 example" do
-    input = """
-    1000
-    2000
-    3000
-
-    4000
-
-    5000
-    6000
-
-    7000
-    8000
-    9000
-
-    10000
-    """
-
-    assert Day01.find_most_calories(input) == 24000
+    assert Day01.find_most_calories(@example_input) === 24000
   end
 
   test "day 1 part 1" do
     {:ok, content} = File.read("test/day01_input.txt")
-    assert Day01.find_most_calories(content) == 68467
+    assert Day01.find_most_calories(content) === 68467
+  end
+
+  test "day 1 part 2 example" do
+    assert Day01.find_most_calories(@example_input, 3) === 45000
   end
 end
