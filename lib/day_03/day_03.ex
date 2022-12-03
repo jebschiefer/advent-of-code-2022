564729ab3edd247c69a1bb69964b981f5e1d1054
Jeb Schiefer <jeb.schiefer@modea.com>
2022-12-03 11:10:32 -0500

    Implement day 3 part 1

--- lib/day_03/day_03.ex
+++ lib/day_03/day_03.ex
@@ -1,5 +1,34 @@
 defmodule Day03 do
   def sum_priorities(input) do
-    0
+    input
+    |> String.split("\n", trim: true)
+    |> Enum.map(&find_duplicate/1)
+    |> Enum.map(&get_character_value/1)
+    |> Enum.reduce(fn x, acc -> x + acc end)
+  end
+
+  def find_duplicate(rucksack) do
+    characters = rucksack |> String.graphemes()
+
+    compartments = Enum.chunk_every(characters, div(length(characters), 2))
+
+    first = List.first(compartments)
+    second = List.last(compartments)
+
+    (first -- first -- second) |> hd
+  end
+
+  def get_character_value(character) do
+    lowercase_offset = 96
+    capital_offset = 38
+
+    lowercase_a = 97
+
+    ascii = character |> String.to_charlist() |> hd
+
+    cond do
+      ascii >= lowercase_a -> ascii - lowercase_offset
+      true -> ascii - capital_offset
+    end
   end
 end
--- test/day_03/day_03_test.exs
+++ test/day_03/day_03_test.exs
@@ -8,6 +8,6 @@ defmodule Day03.Test do
 
   test "day 3 part 1" do
     {:ok, input} = File.read("test/day_03/day_03_input.txt")
-    assert Day03.sum_priorities(input) === 157
+    assert Day03.sum_priorities(input) === 8349
   end
 end
