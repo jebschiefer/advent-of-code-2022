f6f05f9c3900d7239cb009a57c1fe767dc7b5646
Jeb Schiefer <jeb.schiefer@modea.com>
2022-12-04 10:06:39 -0500

    Add test for day 4 part 1 solution

--- test/day_04/day_04_test.exs
+++ test/day_04/day_04_test.exs
@@ -10,10 +10,10 @@ defmodule Day04.Test do
     assert Day04.count_covered_assignments(input) === 2
   end
 
-  # test "day 4 part 1" do
-  #   {:ok, input} = File.read("test/day_04/day_04_input.txt")
-  #   assert Day04.count_covered_assignments(input) === -1
-  # end
+  test "day 4 part 1" do
+    {:ok, input} = File.read("test/day_04/day_04_input.txt")
+    assert Day04.count_covered_assignments(input) === 599
+  end
 
   ###
   # Unit tests
